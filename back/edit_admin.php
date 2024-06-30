c693541b4c0bd6cf44e3eb191d87d08e58c31773
HannaWang003 <hannawang003@gmail.com>
2024-06-30 21:05:45 +0800

    done:帳號相關完成

--- back/edit_admin.php
+++ back/edit_admin.php
@@ -1,7 +1,7 @@
 <style>
-    th {
-        width: 40%;
-    }
+th {
+    width: 40%;
+}
 </style>
 <?php
 $row = $Admin->find($_GET['id'])
@@ -15,16 +15,21 @@ $row = $Admin->find($_GET['id'])
         </tr>
         <tr>
             <th class="tt">密碼</th>
-            <td class="pp"><input type="password" name="pw" value="<?= str_repeat("*", mb_strlen($row['pw'])) ?>"></td>
+            <td class="pp"><input type="password" name="pw" value="<?$row['pw']?>"></td>
         </tr>
         <tr>
             <th class="tt">權限</th>
             <td class="pp">
-                <input type="checkbox" name="pr[]" value="1" <?= (in_array(1, unserialize($row['pr']))) ? "checked" : "" ?>>商品分類與管理<br>
-                <input type="checkbox" name="pr[]" value="2" <?= (in_array(2, unserialize($row['pr']))) ? "checked" : "" ?>>訂單管理<br>
-                <input type="checkbox" name="pr[]" value="3" <?= (in_array(3, unserialize($row['pr']))) ? "checked" : "" ?>>會員管理<br>
-                <input type="checkbox" name="pr[]" value="4" <?= (in_array(4, unserialize($row['pr']))) ? "checked" : "" ?>>頁尾版權區管理<br>
-                <input type="checkbox" name="pr[]" value="5" <?= (in_array(5, unserialize($row['pr']))) ? "checked" : "" ?>>最新消息管理<br>
+                <input type="checkbox" name="pr[]" value="1"
+                    <?= (in_array(1, unserialize($row['pr']))) ? "checked" : "" ?>>商品分類與管理<br>
+                <input type="checkbox" name="pr[]" value="2"
+                    <?= (in_array(2, unserialize($row['pr']))) ? "checked" : "" ?>>訂單管理<br>
+                <input type="checkbox" name="pr[]" value="3"
+                    <?= (in_array(3, unserialize($row['pr']))) ? "checked" : "" ?>>會員管理<br>
+                <input type="checkbox" name="pr[]" value="4"
+                    <?= (in_array(4, unserialize($row['pr']))) ? "checked" : "" ?>>頁尾版權區管理<br>
+                <input type="checkbox" name="pr[]" value="5"
+                    <?= (in_array(5, unserialize($row['pr']))) ? "checked" : "" ?>>最新消息管理<br>
             </td>
         </tr>
     </table>
