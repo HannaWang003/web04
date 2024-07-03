b78e9d4f01f5ebc9be25b5648c7867287a41b821
HannaWang003 <hannawang003@gmail.com>
2024-07-03 10:57:01 +0800

    done:前台menu

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 include_once "./api/db.php";
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -42,15 +43,36 @@ include_once "./api/db.php";
         </div>
         <div id="left" class="ct">
             <div style="min-height:400px;">
+                <div class="ww"><a href="?type=0">全部商品(<?= $Goods->count(['sh' => 1]) ?>)</a></div>
+                <?php
+                $bigs = $Type->all(['big_id' => 0]);
+                foreach ($bigs as $big) {
+                ?>
+                <div class="ww"><a
+                        href="?type=<?= $big['id'] ?>"><?= $big['name'] ?>(<?= $Goods->count(['big' => $big['id'], 'sh' => 1]) ?>)</a>
+                    <?php
+                    if ($Type->count(['big_id' => $big['id']]) > 0) {
+                        $mids = $Type->all(['big_id' => $big['id']]);
+                        foreach ($mids as $mid) {
+                    ?>
+                    <div class="s"><a
+                            href="?type=<?= $mid['id'] ?>"><?= $mid['name'] ?>(<?= $Goods->count(['sh' => 1, 'mid' => $mid['id']]) ?>)</a>
+                    </div>
+                    <?php
+                        }
+                    }
+                 echo "</div>";
+                }
+                ?>
+                </div>
+                <span>
+                    <div>進站總人數</div>
+                    <div style="color:#f00; font-size:28px;">
+                        00005 </div>
+                </span>
             </div>
-            <span>
-                <div>進站總人數</div>
-                <div style="color:#f00; font-size:28px;">
-                    00005 </div>
-            </span>
-        </div>
-        <div id="right">
-            <?php
+            <div id="right">
+                <?php
             $do = ($_GET['do']) ?? "main";
             $file = "./front/$do.php";
             if (file_exists(($file))) {
@@ -59,10 +81,10 @@ include_once "./api/db.php";
                 include "./front/main.php";
             }
             ?>
+            </div>
+            <div id="bottom" style="line-height:70px;background:url(./icon/bot.png); color:#FFF;" class="ct">
+                <?= $Bottom->find(1)['bottom'] ?></div>
         </div>
-        <div id="bottom" style="line-height:70px;background:url(./icon/bot.png); color:#FFF;" class="ct">
-            <?= $Bottom->find(1)['bottom'] ?></div>
-    </div>
 
 </body>
 
