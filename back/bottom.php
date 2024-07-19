b0bdd100d262e151b3eebd2f390309033f28e0a3
HannaWang003 <hannawang003@gmail.com>
2024-07-19 10:44:33 +0800

    problem

---
+++ back/bottom.php
@@ -0,0 +1,19 @@
+<?php
+$table = $_GET['do'];
+$DB = ${ucfirst($table)};
+if (isset($_POST['bottom'])) {
+    $DB->save($_POST);
+}
+?>
+<h1 class="ct">編輯頁尾版權區</h1>
+<form action="?do=bottom" method="post">
+    <table class="all">
+        <tr>
+            <th class="tt">頁尾宣告內容</th>
+            <td class="pp"><input type="text" name="bottom" value="<?= $DB->find(1)['bottom'] ?>" style="width:90%">
+            </td>
+        </tr>
+    </table>
+    <input type="hidden" name="id" value="1">
+    <div class="ct"><input type="submit" value="編輯"><input type="reset" value="重置"></div>
+</form>
